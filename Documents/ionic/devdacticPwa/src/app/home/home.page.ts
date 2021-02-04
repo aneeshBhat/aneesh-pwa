5119f470bc651370223341bd203faf62ae0fd473
AneeshbhatG <aneesh.bhat@cashapona.com>
2021-02-04 12:53:59 +0530

    Initial commit.

---
+++ Documents/ionic/devdacticPwa/src/app/home/home.page.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+const { Network } = Plugins;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  users = [];
+  joke = null;
+  appIsOnline = true;
+
+  constructor(private http: HttpClient) {}
+
+  async ngOnInit() {
+    const status = await Network.getStatus();
+    this.appIsOnline = status.connected;
+ 
+    Network.addListener('networkStatusChange', (status) => {
+      this.appIsOnline = status.connected;
+      console.log(this.appIsOnline)
+    });
+  }
+
+  getData() {
+    this.http.get('https://randomuser.me/api/?results=5').subscribe(result => {
+      console.log('results: ', result);
+      this.users = result['results'];
+    });
+  }
+ 
+  getOnlineData() {
+    this.http.get('https://api.chucknorris.io/jokes/random').subscribe(result => {
+      console.log('joke result: ', result);
+      this.joke = result;
+    });
+  }
+ 
+
+}
